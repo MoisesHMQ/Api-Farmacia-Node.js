a6683e72f52dbb2239049a028c82030a0a35286a
MoisesHMQ <mendes.moisesmgs@gmail.com>
2022-07-29 09:53:17 -0300

    endpoint cadastro de medicamentos

---
+++ Api_Farmacia.js
@@ -0,0 +1,21 @@
+const { request, response } = require('express');
+const express = require('express')
+const app = express();
+var uuid = require('uuid');
+
+app.use(express.json());
+
+const medicamentos = []
+
+app.post('/medicamentos/cadastro', (request, response) => {
+    const validarRemedios = usuarios.find((validar) => validar.codigo == request.body.codigo)
+        if (validarRemedios){
+            return response.send("Erro: Remedio já cadastrado.")}
+        
+        medicamentos.push({
+        id: uuid.v4(),
+        codigo: request.body.codigo,
+        remedio: request.body.remedio
+    })
+    return response.send("Status: Medicamento criado com sucesso.")
+})
